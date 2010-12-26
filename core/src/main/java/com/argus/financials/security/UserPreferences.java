406dd41836b316e2ca32514ce31d12b250e6916d
shibaevv <shibaevv@65940d0d-0698-491e-b608-6f35d31c8762>
2010-12-26 02:59:21 +0000

    userPreferences added to spring config.
    git-svn-id: http://svn.apollosoft.net.au:18080/svn/myplanner/trunk@31 65940d0d-0698-491e-b608-6f35d31c8762

---
+++ core/src/main/java/com/argus/financials/security/UserPreferences.java
@@ -0,0 +1,14 @@
+package com.argus.financials.security;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author vchibaev (Valeri SHIBAEV)
+ */
+public class UserPreferences implements Serializable {
+
+    /** serialVersionUID */
+    
+
+}
